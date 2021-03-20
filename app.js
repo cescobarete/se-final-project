00dc9ce39e358cdd29ec31dd86ed448aa2295add
cescobarete <46694783+cescobarete@users.noreply.github.com>
2021-03-19 21:02:13 -0500

    IDK

--- app.js
+++ app.js
@@ -18,5 +18,9 @@ app.get('', (req, res) => {
     res.render('index', {text: 'This is ejs'})
 })
 
+app.get('', (req, res) => {
+    res.render('index', {text: 'This is ejs'})
+})
+
 // Listeen on port 3000
 app.listen(port, () => console.info('Listening on port ${port}'))
