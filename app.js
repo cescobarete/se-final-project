4c8d7ee3253f6687c1cf1baf516d16524b88d328
cescobarete <46694783+cescobarete@users.noreply.github.com>
2021-03-20 16:50:11 -0500

    IDK

--- app.js
+++ app.js
@@ -9,17 +9,8 @@ app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
 app.use('/img', express.static(__dirname + 'public/img'))
 
-// Set views
-app.set('views', './views')
-app.set('view engine', 'ejs')
-
-
 app.get('', (req, res) => {
-    res.render('index', {text: 'This is ejs'})
-})
-
-app.get('', (req, res) => {
-    res.render('index', {text: 'This is ejs'})
+    res.sendFile(__dirname + '/views/index.html')
 })
 
 // Listeen on port 3000
