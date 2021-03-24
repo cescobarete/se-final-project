5747a794e095d26a261f48da005468715ebd9268
cescobarete <46694783+cescobarete@users.noreply.github.com>
2021-03-24 17:07:12 -0500

    Fixed

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# se-final-project
+# express-node2
--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const port = 3000
 
 // static Files
 app.use(express.static('./public'))
-//app.use('/js',express.static(__dirname + '/js'))
-//app.use('/css',express.static(__dirname + '/css'))
-//app.use('/img',express.static(__dirname + '/img'))
+//app.use('/course',express.static(__dirname + '/course'))
+//app.use('/regexp',express.static(__dirname + '/regexp'))
+//app.use('/landing',express.static(__dirname + '/landing'))
+//app.use('/dice',express.static(__dirname + '/dice'))
 
 // Listeen on port 3000
-app.listen(port, () => console.info('Listening on port ${port}'))
+app.listen(port, () => console.info('Listening on port 3000'))
