60300eb862a534878be31d05e9018d6c32cf0ddb
cescobarete <46694783+cescobarete@users.noreply.github.com>
2021-03-24 20:53:09 -0500

    Fixed

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const port = 3000
 
 // static Files
 app.use(express.static('./public'))
+//app.use('/course',express.static(__dirname + '/course'))
+//app.use('/regexp',express.static(__dirname + '/regexp'))
+//app.use('/landing',express.static(__dirname + '/landing'))
+//app.use('/dice',express.static(__dirname + '/dice'))
 
 // Listeen on port 3000
-app.listen(port, () => console.info('Listening on port 3000'))
+app.listen(port, () => console.info('Listening on port ${port}'))
